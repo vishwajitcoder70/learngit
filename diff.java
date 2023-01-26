b93a5b37ac2f639d9abb5000dd4eedb0f1b8d435
Vishwajit singh <vsinghguleria@gmail.com>
2023-01-26 17:11:03 +0530

    commiting after updation

--- diff.java
+++ diff.java
@@ -1,2 +1,4 @@
-package PACKAGE_NAME;public class diff {
+public class diff {
+
+    
 }
--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum {
     public static void main(String[] args) {
         int a=5;
         int b=6;
-        System.out.println(a+b);
+        System.out.println("this is the sum"+(a+b));
     }
 }
